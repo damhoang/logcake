86326219483bfcf77626257282ab06a2b65a1b4a
damhoang <38296039+damhoang@users.noreply.github.com>
2022-11-09 19:35:19 -0600

    fix ES lint errors

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,24 +1,9 @@
 import React from "react";
 
 import { Container } from "@mui/material";
-import CakeCard from "../components/CakeCard";
-import { createStyles, makeStyles } from "@mui/styles";
-import Grid from "@mui/material/Grid";
-import { Typography } from "@mui/material";
 import OrderForm from "../components/OrderForm";
 
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    card: {
-      margin: "0 auto",
-      padding: "5px 5px",
-    },
-    button: { marginLeft: "5px", marginRight: "5px" },
-  })
-);
-
 export default function Order() {
-  const classes = useStyles();
   return (
     <>
       <Container fixed>
